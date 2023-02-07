77c8df80bd742aa2235f9567d004ca37fb2f30cb
tcude <tanner4137@gmail.com>
2023-02-07 11:46:15 -0600

    Less hard coded values

--- qm_migrate.sh
+++ qm_migrate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "###################################"
-echo "Welcome to the qm migrate script!"
+echo "Welcome to the qm migrate script"
 echo -e "################################### \n"
 echo "Please start by entering the numerical ID of"
 echo -e "the VM you would like to migrate: \n"
 read vm_id
 echo -e "       \n"
-echo -e "We will migrate VM $vm_id \n"
+echo -e "VM $vm_id will be migrated \n"
 echo "Next, please enter the name of the"
 echo "target host that you would like to"
 echo -e "migrate the VM to. Your choices are: \n"
@@ -17,9 +17,8 @@ echo -e "       \n"
 echo -e "VM $vm_id will be migrated to $target_hostname \n"
 echo "Lastly, please supply the name of the"
 echo -e "datastore you would like to migrate to \n"
-echo "- Pluto: 'zfs'"
-echo "- Lola: 'zfs01'"
-echo -e "- Tweety: 'local-zfs' \n"
+pvesm status
+echo -e "       \n"
 read target_datastore
 echo -e "       \n"
 echo "To confirm, you will be migrating VM $vm_id to $target_hostname on datastore $target_datastore"
